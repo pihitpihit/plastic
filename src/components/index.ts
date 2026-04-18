9bb6aa77688cfec04357704517e89869c3d77c77
pihit <pihitpihit@gmail.com>
2026-04-19 05:02:24 +0900

    feat(dialog): components/index.ts에 Dialog export 추가
    Closes #74

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ export * from "./Actionable";
 export * from "./Button";
 export * from "./Card";
 export * from "./CodeView";
+export * from "./Dialog";
 export * from "./PathInput";
 export * from "./Toast";
